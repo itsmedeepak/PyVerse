06ae8cdcfdc670716e9521873a29cdd8ba19db6f
Maryam Shaikh <mshaikh19@gmail.com>
2025-10-18 13:28:21 +0530

    Changes made to QR.py - Using qrcode libraray instead of pyqrcode

--- Qr-generator/QR.py
+++ Qr-generator/QR.py
@@ -1,8 +1,26 @@
-import pyqrcode
-import png
-from pyqrcode import QRCode
+# import pyqrcode
+# import png
+# from pyqrcode import QRCode
 
-s = input("Enter Url Of Website, To create QR Code")
-url = pyqrcode.create(s)
-url.svg("myqr.svg", scale = 8)
-url.png('myqr.png', scale = 6)
+import qrcode
+import os
+
+# Using PIL to save the image of the QR that is generated
+from PIL import Image
+
+"""
+    data : URL that is provided
+    filename : name of the file that is saved
+    box_size : size of the box of the QR
+    border : thickness of the border
+"""
+def generateQR(data: str, filename: str, box_size: int = 10, border: int = 4):
+    # Handling case where URL is not provided
+    if not data:
+        print("URL Not Found ! Please Enter a valid URL!")
+        return
+
+# s = input("Enter Url Of Website, To create QR Code")
+# url = pyqrcode.create(s)
+# url.svg("myqr.svg", scale = 8)
+# url.png('myqr.png', scale = 6)
